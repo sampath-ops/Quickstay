64ca34437f5d6573f84b6d370601d29b8d0c9b73
sampath-ops <sampath@crayosa.com>
2021-12-10 14:35:00 +0530

    get-toknow and hero button altered for mobile view

--- src/Components/Home/Hero/SecondSearchbar.js
+++ src/Components/Home/Hero/SecondSearchbar.js
@@ -3,8 +3,8 @@ const SecondSearchbar = () => {
         <div className="second-searchbar" >
             <div>
                 {/* <input type="text"  placeholder="Do you own a residential property? List it here..."/> */}
-                <button>Do you own a residential property? List it here...</button>
-                <button>List Your Property...</button>
+                <button>Do you own a residential property? List it here</button>
+                <button>List Your Property</button>
                 <div className="circle">
                     <i className="fas fa-chevron-right"></i>
                 </div>
