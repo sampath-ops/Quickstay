0554a72941a3b09b6e7ff687056ce169556f2fac
sampath-ops <sampath@crayosa.com>
2021-12-28 21:34:08 +0530

    float right part in details page

--- src/Components/PropertyDetails/PropertyContact/PropertyContact.js
+++ src/Components/PropertyDetails/PropertyContact/PropertyContact.js
@@ -1,10 +1,31 @@
 import './PropertyContact.css';
 import PropertyAddress from './PropertyAddress';
 import PropertySharing from './PropertySharing';
-import ContactButton from './ContactButton'
+import ContactButton from './ContactButton';
+import React, { useEffect, useState } from 'react';
 const PropertyContact = () => {
+
+   // const [offset, setOffset] = useState(0);
+   // useEffect(() => {
+   //    window.onscroll = () => {
+   //       setOffset(window.pageYOffset)
+   //    }
+   // }, []);
+
+   const [float, setFloat] = useState(false);
+   useEffect(() => {
+     window.addEventListener("scroll", () => {
+         setFloat(window.scrollY > 132);
+     });
+//      return () => {
+//       // remove state when unmount
+//       setFloat(false);
+//   };
+   }, []);
+
+
     return ( 
-        <div className="propertycontact">
+        <div className={`propertycontact ${float ? "float-rightpart": ""}`}>
            <PropertyAddress/>
            <PropertySharing/>
            <ContactButton/>
