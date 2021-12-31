b3536a3e6409815c7bccb3e04df511bda152bac6
sampath-ops <sampath@crayosa.com>
2021-12-31 15:24:31 +0530

    get documents based on geohash is implemented

--- src/GeoLocation.js
+++ src/GeoLocation.js
@@ -1,16 +1,55 @@
 import React, { Component } from "react";
+import db from './firebase.config';
+import geohash from "ngeohash";
+// Calculate the upper and lower boundary geohashes for
+// a given latitude, longitude, and distance in miles
+const getGeohashRange = (
+  latitude,
+  longitude,
+  distance, // miles
+) => {
+  const lat = 0.0144927536231884; // degrees latitude per mile
+  const lon = 0.0181818181818182; // degrees longitude per mile
+
+  const lowerLat = latitude - lat * distance;
+  const lowerLon = longitude - lon * distance;
+
+  const upperLat = latitude + lat * distance;
+  const upperLon = longitude + lon * distance;
+
+  const lower = geohash.encode(lowerLat, lowerLon);
+  const upper = geohash.encode(upperLat, upperLon);
+
+  return {
+    lower,
+    upper
+  };
+};
 
 var options = {
   enableHighAccuracy: true,
   timeout: 5000,
   maximumAge: 0,
 };
+
 function success(pos) {
   var crd = pos.coords;
   console.log("Your current position is:");
   console.log(`Latitude : ${crd.latitude}`);
   console.log(`Longitude: ${crd.longitude}`);
   console.log(`More or less ${crd.accuracy} meters.`);
+  const { latitude, longitude } = crd;
+  const range = getGeohashRange(11.642755911349438, 78.16196434142107, 0); // NEED TO USE USER LATLNG
+
+  db
+    .collection("properties")
+    .where("geohash", ">=", range.lower)
+    .where("geohash", "<=", range.upper)
+    .onSnapshot(snapshot => {
+      snapshot.docs.map(doc=>{
+        console.log(doc.data());
+      })
+    })
 }
 
 function errors(err) {
@@ -24,12 +63,12 @@ export default class GeoLocation extends Component {
         .query({ name: "geolocation" })
         .then(function (result) {
           if (result.state === "granted") {
-            console.log(result.state);
-            //If granted then you can directly call your function here
             navigator.geolocation.getCurrentPosition(success);
-          } else if (result.state === "prompt") {
+          } 
+          else if (result.state === "prompt") {
             navigator.geolocation.getCurrentPosition(success, errors, options);
-          } else if (result.state === "denied") {
+          } 
+          else if (result.state === "denied") {
             //If denied then you have to show instructions to enable location
           }
           result.onchange = function () {
