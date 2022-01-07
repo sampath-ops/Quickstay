63add0edb89b833e1e61641c8dc53e46e070c076
samapth-ops <sampathkaali002@gmail.com>
2022-01-07 13:11:45 +0530

    google places api added

--- src/Components/Home/Hero/SearchBar.js
+++ src/Components/Home/Hero/SearchBar.js
@@ -1,9 +1,61 @@
 import './Hero.css';
+import {useState,useEffect,useRef} from 'react';
+
+let autoComplete;
+
+const loadScript = (url, callback) => {
+    let script = document.createElement("script");
+    script.type = "text/javascript";
+
+    if (script.readyState) {
+        script.onreadystatechange = function() {
+        if (script.readyState === "loaded" || script.readyState === "complete") {
+            script.onreadystatechange = null;
+            callback();
+        }
+        };
+    } else {
+        script.onload = () => callback();
+    }
+
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+};
+
+function handleScriptLoad(updateQuery, autoCompleteRef) {
+    autoComplete = new window.google.maps.places.Autocomplete(
+        autoCompleteRef.current,
+        { types: ["(cities)"], componentRestrictions: { country: "in" } }
+    );
+    autoComplete.setFields(["address_components", "formatted_address"]);
+    autoComplete.addListener("place_changed", () =>
+        handlePlaceSelect(updateQuery)
+    );
+}
+
+async function handlePlaceSelect(updateQuery) {
+    const addressObject = autoComplete.getPlace();
+    const query = addressObject.formatted_address;
+    updateQuery(query);
+    console.log(addressObject);
+}
+
 const SearchBar = () => {
+
+    const [query, setQuery] = useState("");
+    const autoCompleteRef = useRef(null);
+
+    useEffect(() => {
+        loadScript(
+        `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&libraries=places`,
+        () => handleScriptLoad(setQuery, autoCompleteRef)
+        );
+    }, []);
+
     return ( 
         <div className="searchbar">
-            
-            <input type="text" placeholder="Search Locality..." />
+            <input type="text" placeholder="Search Locality..." ref={autoCompleteRef}
+            onChange={event => setQuery(event.target.value)} value={query}/>
             <button className="search_icon"><i className="fas fa-search"></i></button>
             <button className='search_text'>SEARCH</button>
         </div>
