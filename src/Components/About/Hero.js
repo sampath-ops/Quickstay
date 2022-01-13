3bdf2c5a05a290ce20998e7f1cd6bad11665d48e
Garvit Chaurasiya <garvitchaurasiya725@gmail.com>
2022-01-13 21:01:34 +0200

    About Page mobile view

--- src/Components/About/Hero.js
+++ src/Components/About/Hero.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import global from '../../WebsiteMaterial/AboutImages/Global.png'
 
 export default function Hero() {
     return (
         <div className="heroSection">
+
             <div className="heroText">
                 <span>What are we doing?</span>
-                <p>Making the rental home search easier!</p>
-            </div>
-            <div className="heroImg">
-                <img src={global} alt="global" />
+                <p>Making the rental home search<br/> easier!</p>
             </div>
         </div>
     )
